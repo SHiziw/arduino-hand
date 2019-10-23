bb02bf0c8e3aac7fd0567ec0db055b553bd7b04c
SHiziw <45631851+SHiziw@users.noreply.github.com>
2019-10-23 17:07:39 +0800

    Initial commit

---
+++ soft_hand_control_for_contest.ino/test.ino
@@ -0,0 +1,41 @@
+
+void extend5tg(){
+
+
+}
+
+void shenzhan5ob(){
+
+
+}
+
+void contract5tg(){
+
+
+}
+
+void contract5ob(){
+
+
+}
+
+void contractThumb(){
+
+
+}
+
+
+
+class gastube{
+    void releasePressure(){
+
+        
+    }
+
+
+    void halt(){
+
+
+
+    }
+};
