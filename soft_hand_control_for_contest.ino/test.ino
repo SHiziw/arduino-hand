406de291e08d8be6c6dc493ca7f94951070a859b
SHiziw <45631851+SHiziw@users.noreply.github.com>
2019-10-24 19:14:59 +0800

    init

--- soft_hand_control_for_contest.ino/test.ino
+++ soft_hand_control_for_contest.ino/test.ino
@@ -12,9 +12,100 @@ int ledPins[] = {
     5,
     4,
 }; // 对应的led引脚
+
+
+
+
+
+class controlBase{
+public:
+int place[10];
+
+void turnOn(int pin, int plc){          //turn on port pin, on status plc.
+  Serial.println("turnOn");
+  if(plc == 1){
+    digitalWrite(ledPins[pin], HIGH); 
+  }else digitalWrite(ledPins[pin], LOW);
+}
+
+void handleCmd(int series){   //get the command code, like 1010101010, from port 1 to 10.
+  place[0] = series / 1 % 10;
+  place[1] = series / 10 % 10;
+  place[2] = series / 100 % 10;
+  place[3] = series / 1000 % 10;
+  place[4] = series / 10000 % 10;
+  place[5] = series / 100000 % 10;
+  place[6] = series / 1000000 % 10;
+  place[7] = series / 10000000 % 10;
+  place[8] = series / 100000000 % 10;
+  place[9] = series / 1000000000 % 10;  
+  for(int plc=0; plc++; plc<ledCount){
+    turnOn(plc, place[plc]);
+  }
+}
+
+void hangOn(double time){
+  delay(time);
+}
+
+};
+
+
+
+class TainingOne:controlBase{
+public:
+void releasePressure(int peroid){ /* 001排气 */
+handleCmd(1111111000);
+hangOn(peroid);      
+}
+
+void halt(int peroid){       /*002关停*/
+handleCmd(0000000000);
+hangOn(peroid);
+}
+
+
+void extend5tg(int peroid){   /*五指同时伸展11*/
+handleCmd(0000000000);
+hangOn(peroid);
+
+}
+
+
+
+void contract5tg(int peroid){  /*五指同时屈曲12*/
+handleCmd(0000000000);
+hangOn(peroid);
+
+}
+void extend1ob(int peroid){     /*单指依次伸展13*/
+handleCmd(0000000000); //1
+hangOn(peroid);
+handleCmd(0000000000); //2
+hangOn(peroid);
+handleCmd(0000000000);  //3
+hangOn(peroid);
+handleCmd(0000000000);  //4
+hangOn(peroid);
+
+}
+void contract5ob(){
+
+
+}
+
+void contractThumb(){
+
+
+}
+};
+
 void setup()
 {
-
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
   for (int thisLed = 0; thisLed < ledCount; thisLed++)
   {
     pinMode(ledPins[thisLed], OUTPUT); // 循环设置，把对应的led都设置成输出
@@ -24,7 +115,13 @@ void setup()
 void loop()
 {
 
-  for (int num = 0; num < ledCount; num++) // 熄灭所有led，不同的电路连接也会得到点亮所有led
+
+
+
+  TainingOne test;
+  test.halt(1000);
+
+  /*for (int num = 0; num < ledCount; num++) // 熄灭所有led，不同的电路连接也会得到点亮所有led
   {
     digitalWrite(ledPins[num], HIGH);
   }
@@ -34,61 +131,5 @@ void loop()
     digitalWrite(ledPins[num], LOW); // 循环顺序点亮led然后等待200ms后熄灭
     delay(200);
     digitalWrite(ledPins[num], HIGH);
-  }
+  }*/
 }
-
-
-
-
-class controlSum{
-void trunOn(int series){
-
-}
-
-void hangOn(double time){
-    delay(time);
-}
-
-};
-
-
-
-class gasTubeControl{
-    void releasePressure(){ // 001排气
-
-        
-    }
-
-    void halt(){       //002关停
-
-
-
-    }
-
-    
-    void extend5tg(){   //五指同时伸展11
-
-
-    }
-
-    void extend5ob(){   
-
-
-    }
-
-    void contract5tg(){  //五指同时屈曲12
-
-
-    }
-
-    void contract5ob(){
-
-
-    }
-
-    void contractThumb(){
-
-
-    }
-};
-
