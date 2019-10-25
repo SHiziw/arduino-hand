c87ee5d5bd62d93b72b8012b22f6a1d41098492c
SHiziw <45631851+SHiziw@users.noreply.github.com>
2019-10-25 14:49:16 +0800

    finished stop cmd.

--- soft_hand_control_for_contest.ino/test.ino
+++ soft_hand_control_for_contest.ino/test.ino
@@ -1,5 +1,6 @@
 const int ledCount = 10; // led总共的个数
 String comdata = "";
+int stp = 0;
 
 int ledPins[] = {
     13,
@@ -22,7 +23,7 @@ int place[10];
 void turnOn(int pin, int plc){          //turn on port pin, on status plc.
   if(plc == 1){
     Serial.print(pin);
-    Serial.println("is HIGH");
+    Serial.println(" turns on.");
     digitalWrite(ledPins[pin], LOW); 
   }else digitalWrite(ledPins[pin], HIGH);
 }
@@ -39,7 +40,6 @@ void handleCmd(long *series){   //get the command code, like 1010101010, from po
   place[1] = series[1];
   place[0] = series[0];
   Serial.println("cmd is working");
-
   /*place[0] = series / 1 % 10;
   place[1] = series / 10 % 10;
   place[2] = series / 100 % 10;
@@ -51,12 +51,30 @@ void handleCmd(long *series){   //get the command code, like 1010101010, from po
   place[8] = series / 100000000 % 10;
   place[9] = series / 1000000000 % 10;*/  
   for(int plc=0; plc<ledCount; plc++){
-    turnOn(plc, place[plc]);
+    while(!stp){
+      turnOn(plc, place[plc]);
+      break;
+    }    
   }
 }
 
 void hangOn(double time){
-  delay(time);
+  while(!stp){
+    delay(time);
+    break;
+  }
+  while (Serial.available() > 0)  
+  {
+      comdata += char(Serial.read());
+      delay(2);
+  }
+  if (comdata.length() > 0){
+    if (comdata.equals("0050\r\n")){ //训练5
+      comdata = "";
+      stp = 1;
+    }
+    comdata = "";
+  }
 }
 
 void f001(double rate){ /* 001排气 */
@@ -236,13 +254,14 @@ void letOn2043(double rate){
 class TrainingFive:controlBase{
 public:
 void letOn(double rate){
+stp = 0;
 f001(1.0);
 f002(1.0);
 }
 };
 
 void setup()
-{
+{ stp = 0;
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
@@ -260,10 +279,10 @@ TrainingTwo test2;
 TrainingThree test3;
 TrainingFour test4;
 TrainingFive test5;
-String comdat = "11";
-    if (comdat.equals("101")) {
-      Serial.println(comdat);
-    }
+  if (stp){ //训练5
+   // comdata = "";
+    test5.letOn(1.0);
+  }
 
   while (Serial.available() > 0)  
   {
@@ -272,45 +291,53 @@ String comdat = "11";
   }
     if (comdata.length() > 0){
       if (comdata.equals("0010\r\n")){ //训练1
-      Serial.println("recieved0010");
+        comdata = "";
         test1.letOn(1.0, 3);
       }
       if (comdata.equals("0021\r\n")){ //训练2
+        comdata = "";
         test2.letOn21(1.0);
       }
         if (comdata.equals("0022\r\n")){ //训练2
+        comdata = "";
         test2.letOn22(1.0);
       }
       if (comdata.equals("0030\r\n")){ //训练3
+        comdata = "";
         test3.letOn(1.0);
       }
       if (comdata.equals("0040\r\n")){ //训练4        
+        comdata = "";
         test4.letOn(1.0);
       }
       if (comdata.equals("1041\r\n")){ //训练4
+        comdata = "";
         test4.letOn1041(1.0);
       }
       if (comdata.equals("1042\r\n")){ //训练4
+        comdata = "";
         test4.letOn1042(1.0);
       }
       if (comdata.equals("1043\r\n")){ //训练4
+        comdata = "";
         test4.letOn1043(1.0);
       }      
       if (comdata.equals("2041\r\n")){ //训练4
+        comdata = "";
         test4.letOn2041(1.0);
       }
       if (comdata.equals("2042\r\n")){ //训练4
+        comdata = "";
         test4.letOn2042(1.0);
       }
       if (comdata.equals("2043\r\n")){ //训练4
+        comdata = "";
         test4.letOn2043(1.0);
-      }
-      if (comdata.equals("0050\r\n")){ //训练5
-        test5.letOn(1.0);
-      }
+      }      
       comdata = "";
     }
-  
+  Serial.println("loop on");
+  delay(2);
 
 
 
